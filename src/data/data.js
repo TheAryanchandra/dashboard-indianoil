b7cbb5e7c71c31b91203b9591f595d45bb7cdd3a
Aryan chandra <90573146+TheAryanchandra@users.noreply.github.com>
2024-08-24 10:19:50 +0530

    Dashboard

---
+++ src/data/data.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { AiOutlineCalendar, AiOutlineShoppingCart, AiOutlineAreaChart, AiOutlineBarChart, AiOutlineStock } from 'react-icons/ai';
+import { FiShoppingBag, FiEdit, FiPieChart, FiBarChart, FiCreditCard, FiStar, FiShoppingCart } from 'react-icons/fi';
+import { BsKanban, BsBarChart, BsBoxSeam, BsCurrencyDollar, BsShield, BsChatLeft } from 'react-icons/bs';
+import { BiColorFill } from 'react-icons/bi';
+import { IoMdContacts } from 'react-icons/io';
+import { RiContactsLine, RiStockLine } from 'react-icons/ri';
+import { MdOutlineSupervisorAccount } from 'react-icons/md';
+import { HiOutlineRefresh } from 'react-icons/hi';
+import { TiTick } from 'react-icons/ti';
+import { GiLouvrePyramid } from 'react-icons/gi';
+import { GrLocation } from 'react-icons/gr';
+
+
+const fs = require('fs');
+
+// The data you want to append to the file
+const dataToAdd = "This is the new data that will be added.\n";
+
+// The file you want to add data to
+const filePath = "Abhilekh.xlsx";
+
+// Append data to the file
+fs.appendFile(filePath, dataToAdd, (err) => {
+  if (err) {
+    console.error("An error occurred while adding data to the file:", err);
+  } else {
+    console.log("Data successfully added to the file!");
+  }
+});
+
+
+export const areaCustomSeries = [
+  {
+    dataSource: areaChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'USA',
+    opacity: '0.8',
+    type: 'SplineArea',
+    width: '2',
+
+  },
+  {
+    dataSource: areaChartData[1],
+    xName: 'x',
+    yName: 'y',
+    name: 'France',
+    opacity: '0.8',
+    type: 'SplineArea',
+    width: '2',
+  },
+  {
+    dataSource: areaChartData[2],
+    xName: 'x',
+    yName: 'y',
+    name: 'Germany',
+    opacity: '0.8',
+    type: 'SplineArea',
+    width: '2',
+  },
+];
+
+export const barChartData = [
+  [
+    { x: 'USA', y: 46 },
+    { x: 'GBR', y: 27 },
+    { x: 'CHN', y: 26 },
+  ],
+  [
+    { x: 'USA', y: 37 },
+    { x: 'GBR', y: 23 },
+    { x: 'CHN', y: 18 },
+  ],
+  [
+    { x: 'USA', y: 38 },
+    { x: 'GBR', y: 17 },
+    { x: 'CHN', y: 26 },
+  ],
+];
+
+export const barCustomSeries = [
+  {
+    dataSource: barChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'Gold',
+    type: 'Column',
+    marker: {
+      dataLabel: {
+        visible: true,
+        position: 'Top',
+        font: { fontWeight: '600', color: '#ffffff' },
+      },
+    },
+  },
+  {
+    dataSource: barChartData[1],
+    xName: 'x',
+    yName: 'y',
+    name: 'Silver',
+    type: 'Column',
+    marker: {
+      dataLabel: {
+        visible: true,
+        position: 'Top',
+        font: { fontWeight: '600', color: '#ffffff' },
+      },
+    },
+  },
+  {
+    dataSource: barChartData[2],
+    xName: 'x',
+    yName: 'y',
+    name: 'Bronze',
+    type: 'Column',
+    marker: {
+      dataLabel: {
+        visible: true,
+        position: 'Top',
+        font: { fontWeight: '600', color: '#ffffff' },
+      },
+    },
+  },
+];
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+          {themeSettings && (<ThemeSettings />)}
